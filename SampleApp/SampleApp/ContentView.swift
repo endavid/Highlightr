5cec4913dcb18642d5bda121d44831241f32cf1a
David Gavilan <david@endavid.com>
2026-01-02 17:10:42 +0000

    Add Metal keywords to cpp language. Sorry not sorry.

--- SampleApp/SampleApp/ContentView.swift
+++ SampleApp/SampleApp/ContentView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 import Highlightr
 
 struct ContentView: View {
-    @State private var text: NSAttributedString
-    
+    @State private var textA: NSAttributedString
+    @State private var textB: NSAttributedString
+
     init() {
         let highlightr = Highlightr()!
         highlightr.setTheme(to: "railscasts")
-        let fontSize: CGFloat = 12
+        let fontSize: CGFloat = 8
         highlightr.theme.codeFont = highlightr.theme.codeFont.withSize(fontSize)
         highlightr.theme.boldCodeFont = highlightr.theme.boldCodeFont.withSize(fontSize)
         highlightr.theme.italicCodeFont = highlightr.theme.italicCodeFont.withSize(fontSize)
-        let code = """
+        let codeA = """
 // Comments may look the wrong font with certain styles
 // like pojoaque, which it's the default, sadly
 let a = 42
 let b = "This is a string"
 """
         // You can omit the second parameter to use automatic language detection.
-        text = highlightr.highlight(code, as: "swift")!
+        textA = highlightr.highlight(codeA, as: "swift")!
+        let codeB = """
+/// This is a Metal Shader example
+fragment half4 main()
+{
+    float pi = 3.1416;
+    float2 uv = frag.uv;
+    float4 a = texA.sample(sam, uv);
+    int2 dummy = int2(0, 1);
+    return half4(a);
+}
+"""
+        textB = highlightr.highlight(codeB, as: "cpp")!
     }
     
     var body: some View {
@@ -33,7 +46,11 @@ let b = "This is a string"
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            AttributedText(attributedString: text)
+            AttributedText(attributedString: textA)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            AttributedText(attributedString: textB)
         }
         .padding()
     }
